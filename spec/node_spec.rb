6bf5530aa488472644a3c248c490bcdbc9dd23fe
Nicholas McEnroe <nsmcenro@gmail.com>
2023-07-07 08:08:32 -0400

    test: add .data test

--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -10,4 +10,12 @@ describe Node do
             expect(node).to be_an_instance_of(Node)
         end
     end
+    describe '#plop' do
+        it "returns the name of the data" do
+            node = Node.new("plop")
+            expect(node.data).to eq("plop")
+        end
+    end
+
+
 end
