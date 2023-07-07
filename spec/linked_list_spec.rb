a149dda0d263d6c784d9f5ea3c75477ceb04d3ed
Nicholas McEnroe <nsmcenro@gmail.com>
2023-07-07 09:05:06 -0400

    Test: Add empty head test

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -6,7 +6,13 @@ describe LinkedList do
     describe '#initialize' do
         it 'is an instance of LinkedList' do
             list = LinkedList.new
-            expect(list).to be be_an_instance_of(LinkedList)
+            expect(list).to be_an_instance_of(LinkedList)
+        end
+    end
+    describe '#head' do 
+        it 'returns a nil when empty' do
+            list = LinkedList.new
+            expect(list.head).to eq(nil)
         end
     end
 end
